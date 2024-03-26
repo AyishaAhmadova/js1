3310b625a3717d20c5ade4c54b49b94093cca1b2
ayishaahmadova <asgarlia29@gmail.com>
2024-03-26 19:16:25 +0400

    js1

---
+++ app.js
@@ -0,0 +1,10 @@
+let ad = prompt("Adinizi daxil edin");
+console.log(ad.length);
+let b = "hello world";
+console.log(b.indexOf("r"));
+console.log(b.startsWith("h"));
+console.log(b.endsWith("d"));
+//indexof2
+let a =prompt( Salam)
+let symbol= promt('S')
+console.log(a.indexof(symbol))
